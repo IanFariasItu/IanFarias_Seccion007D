d6ab01b9198f198a38c5550148c146d8ed63c5af
unknown <ia.fariasi@duocuc.cl>
2021-09-20 03:42:50 -0300

    Se crea la pagina de inicio

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+interface Componente{
+  icon: string;
+  name: string;
+  redirecTo:string;
+
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes : Componente[] =[
+    {
+      icon: 'paw-outline',
+      name: 'Action Sheet', 
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'sunny-outline', 
+      name: 'Alert', 
+      redirecTo: '/alert'
+    },
+  ]
+  constructor(private menuController: MenuController) { }
+
+  ngOnInit() {
+  }
+
+  mostrarMenu(){
+    this.menuController.open('first');
+  }
+
+}
